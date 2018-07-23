79b844219a6851306083eb3f12b4cd4f8d7d9d3f
Rikki407 <rishablamba407@gmail.com>
2018-07-23 13:37:07 +0530

    v4

--- contracts/Rikki_Staterium.sol
+++ contracts/Rikki_Staterium.sol
@@ -3,10 +3,12 @@ contract Rikki_Staterium {
     address owner;
     constructor () public {
         owner = msg.sender;
-        matches.push(Match(1,1,100, 101, 10, 40, 5,50));
-        matches.push(Match(1,1,102, 103, 10, 40, 5,50));
-        matches.push(Match(1,1,104, 105, 10, 40, 5,50));
-        matches.push(Match(1,1,106, 107, 10, 40, 5,50));
+        
+    //Dummy matches 
+        Matches[10] = Match(1, 10, 100, 101, 10, 40, 5, 50);
+        Matches[11] = Match(1, 11, 102, 103, 10, 40, 5, 50);
+        Matches[12] = Match(1, 12, 104, 105, 10, 40, 5, 50);
+        Matches[13] = Match(1, 13, 106, 107, 10, 40, 5, 50);
     }
     modifier onlyOwner(){
         require(msg.sender == owner);
@@ -24,23 +26,27 @@ contract Rikki_Staterium {
         uint256 maxStake; // in ether
         uint256 endTime; // in minuites
         uint256 threshold; // in ether
+        mapping( address => uint256 ) StakedTag;
+        mapping( address => uint256 ) StakedAmount;  
     }
-   
-    Match[] public matches; 
     
-    mapping( uint256 => uint256[] ) public Wave;
+   
+    mapping( uint256 => Match) public Matches; // _matchID => Match
+    
+    mapping( uint256 => uint256[] ) public Wave; // _waveID => tags[]
    
     //Owner Functions
     function createWave(uint256 _waveID, uint256[] _tags) public onlyOwner {
         Wave[_waveID] = _tags;
     }
     function createMatch(uint256 _waveID, uint256 _matchID, uint256 _endTime, uint256 _tagA, uint256 _tagB, uint256 _minStake, uint256 _maxStake, uint256 _threshold) public onlyOwner {
-        matches.push(Match(_waveID, _matchID,  _tagA,  _tagB,  _minStake,  _maxStake, _endTime,  _threshold));
+        Matches[_matchID] = Match(_waveID, _matchID,  _tagA,  _tagB,  _minStake,  _maxStake, _endTime,  _threshold);
     }
    
     //Player Functions
    
     function stake(uint256 _tag, uint256 _waveID, uint256 _matchID) payable public {
+        
     }
    
     function unstake(uint256 _waveID, uint256 _matchID) public  {
@@ -48,5 +54,3 @@ contract Rikki_Staterium {
     }
     function () public payable{}
     
-
-}
